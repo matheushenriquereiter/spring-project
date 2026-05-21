f6a6edbb3378c55267e0e526f3ad70b42c97cd12
matheushenriquereiter <mathenriquereiter@gmail.com>
2026-05-20 21:52:39 -0300

    Added route to save user in database

--- src/main/java/io/github/matheushenriquereiter/project/controller/UserWebController.java
+++ src/main/java/io/github/matheushenriquereiter/project/controller/UserWebController.java
@@ -1,5 +1,7 @@
 package io.github.matheushenriquereiter.project.controller;
 
+import io.github.matheushenriquereiter.project.dto.UserDTO;
+import io.github.matheushenriquereiter.project.model.User;
 import io.github.matheushenriquereiter.project.model.UserForm;
 import io.github.matheushenriquereiter.project.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -43,6 +45,9 @@ public class UserWebController {
     @PostMapping("/register/save")
     public String createUser(Model model, @ModelAttribute() UserForm userForm) {
         model.addAttribute("userForm", userForm);
+
+        UserDTO userDTO = new UserDTO(userForm.getName(), userForm.getEmail());
+        userService.saveUser(userDTO);
 
         return "register-success";
     }
--- src/main/java/io/github/matheushenriquereiter/project/dto/UserDTO.java
+++ src/main/java/io/github/matheushenriquereiter/project/dto/UserDTO.java
@@ -11,4 +11,9 @@ public class UserDTO {
     private Long id;
     private String name;
     private String email;
+
+    public UserDTO(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
